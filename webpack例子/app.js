379b8f6964a87d5444ab9810abbbc56ca2763639
wl11111 <576516012@qq.com>
2017-06-06 22:45:07 +0800

    2017.6.6

---
+++ webpack例子/app.js
@@ -0,0 +1,3 @@
+import def,{fn2,fn1,fn4} from './common/export.js'; //引入export.js文件
+//def代表默认函数（名字任意取）
+console.log(def(),fn1(),fn2(),fn4());
---
+++ webpack例子/common/export.js
@@ -0,0 +1,14 @@
+export function fn1(){
+	return 123; //输出
+}
+export function fn2(){
+	return 456; //输出
+}
+export default function(){
+	return 'aaa'; //输出默认函数
+}
+//另一种写法
+function fn4(){
+  console.log(4444444);
+}
+export {fn4};
